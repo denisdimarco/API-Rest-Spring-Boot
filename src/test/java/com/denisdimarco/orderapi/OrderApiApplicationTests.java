de38727a8bb80b713b26c7a1d1747cdfd4b91e39
denisdimarco <denisdimarcoleda@gmail.com>
2021-11-14 01:04:52 -0300

    Initial commit

---
+++ src/test/java/com/denisdimarco/orderapi/OrderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.denisdimarco.orderapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
