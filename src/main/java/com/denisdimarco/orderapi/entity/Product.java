0e84c9d716f83a6380f43f9fd5f5370830f3e388
denisdimarco <denisdimarcoleda@gmail.com>
2021-11-16 17:54:49 -0300

    Product @Entity working, table created in DB

--- src/main/java/com/denisdimarco/orderapi/entity/Product.java
+++ src/main/java/com/denisdimarco/orderapi/entity/Product.java
@@ -1,13 +1,25 @@
 package com.denisdimarco.orderapi.entity;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
+
+import javax.persistence.*;
 
 @Getter
 @Setter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "products")
 public class Product {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column (name = "name", nullable = false, length = 100)
     private String name;
+
+    @Column(name = "price", nullable = false)
+    private Double price;
 }
