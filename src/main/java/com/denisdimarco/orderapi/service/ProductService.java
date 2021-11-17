9bd3ced3bfe69130db80170d273efd803f316fac
denisdimarco <denisdimarcoleda@gmail.com>
2021-11-17 20:31:15 -0300

    Exceptions Added and try catch to ProductService

--- src/main/java/com/denisdimarco/orderapi/service/ProductService.java
+++ src/main/java/com/denisdimarco/orderapi/service/ProductService.java
@@ -1,8 +1,12 @@
 package com.denisdimarco.orderapi.service;
 
 import com.denisdimarco.orderapi.entity.Product;
+import com.denisdimarco.orderapi.exception.GeneralServiceException;
+import com.denisdimarco.orderapi.exception.NoDataFoundException;
+import com.denisdimarco.orderapi.exception.ValidateServiceException;
 import com.denisdimarco.orderapi.repository.ProductRepository;
 import com.denisdimarco.orderapi.validator.ProductValidator;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -10,6 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Slf4j
 @Service
 public class ProductService {
 
@@ -17,37 +22,71 @@ public class ProductService {
     private ProductRepository productRepository;
 
     public Product findById(Long productId) {
-        return productRepository.findById(productId)
-                .orElseThrow(() -> new RuntimeException("The product does not exist"));
+        try {
+            return productRepository.findById(productId)
+                    .orElseThrow(() -> new NoDataFoundException("The product does not exist"));
+        } catch (ValidateServiceException | NoDataFoundException e) {
+            log.info(e.getMessage(), e);
+            throw e;
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            throw new GeneralServiceException(e.getMessage(), e);
+        }
+
     }
 
     public List<Product> findAll(Pageable page) {
-        return productRepository.findAll(page).toList();
+        try {
+            return productRepository.findAll(page).toList();
+        } catch (ValidateServiceException | NoDataFoundException e) {
+            log.info(e.getMessage(), e);
+            throw e;
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            throw new GeneralServiceException(e.getMessage(), e);
+        }
     }
 
     @Transactional
     public Product save(Product product) {
 
-        ProductValidator.save(product);
+        try {
+            ProductValidator.save(product);
 
-        if (product.getId() == null) {
-            return productRepository.save(product);
+            if (product.getId() == null) {
+                return productRepository.save(product);
+            }
+            Product updatedProduct = productRepository.findById(product.getId())
+                    .orElseThrow(() -> new NoDataFoundException("The product does not exist"));
+
+            updatedProduct.setName(product.getName());
+            updatedProduct.setPrice(product.getPrice());
+            productRepository.save(updatedProduct);
+            return updatedProduct;
+
+        } catch (ValidateServiceException | NoDataFoundException e) {
+            log.info(e.getMessage(), e);
+            throw e;
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            throw new GeneralServiceException(e.getMessage(), e);
         }
-        Product updatedProduct = productRepository.findById(product.getId())
-                .orElseThrow(() -> new RuntimeException("The product does not exist"));
-
-        updatedProduct.setName(product.getName());
-        updatedProduct.setPrice(product.getPrice());
-        productRepository.save(updatedProduct);
-        return updatedProduct;
     }
 
 
     @Transactional
     public void delete(Long productId) {
-        Product product = productRepository.findById(productId)
-                .orElseThrow(() -> new RuntimeException("The product does not exist"));
-        productRepository.delete(product);
+        try {
+            Product product = productRepository.findById(productId)
+                    .orElseThrow(() -> new NoDataFoundException("The product does not exist"));
+            productRepository.delete(product);
+        } catch (ValidateServiceException | NoDataFoundException e) {
+            log.info(e.getMessage(), e);
+            throw e;
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            throw new GeneralServiceException(e.getMessage(), e);
+        }
     }
 
 }
--- src/main/java/com/denisdimarco/orderapi/validator/ProductValidator.java
+++ src/main/java/com/denisdimarco/orderapi/validator/ProductValidator.java
@@ -1,22 +1,23 @@
 package com.denisdimarco.orderapi.validator;
 
 import com.denisdimarco.orderapi.entity.Product;
+import com.denisdimarco.orderapi.exception.ValidateServiceException;
 
 public class ProductValidator {
     public static void save(Product product) {
         if(product.getName() == null || product.getName().trim().isEmpty()) {
-            throw  new RuntimeException("Name field is required");
+            throw new ValidateServiceException("Name field is required");
         }
 
         if(product.getName().length() > 100) {
-            throw new RuntimeException("Name length is too long: (max 100)");
+            throw new ValidateServiceException("Name length is too long: (max 100)");
         }
 
         if(product.getPrice() == null) {
-            throw new RuntimeException("Price field is required");
+            throw new ValidateServiceException("Price field is required");
         }
         if(product.getPrice() < 0) {
-            throw  new RuntimeException("Price ");
+            throw  new ValidateServiceException("Price ");
         }
     }
 }
