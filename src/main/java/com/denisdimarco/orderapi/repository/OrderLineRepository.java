bc1383852f5f5b0e264f7b6657fef8dd33060f4a
denisdimarco <denisdimarcoleda@gmail.com>
2021-11-19 01:39:08 -0300

    Fixed Typo on OrderLineRepo

--- src/main/java/com/denisdimarco/orderapi/repository/OrderLineRepository.java
+++ src/main/java/com/denisdimarco/orderapi/repository/OrderLineRepository.java
@@ -1,8 +1,9 @@
 package com.denisdimarco.orderapi.repository;
 
+import com.denisdimarco.orderapi.entity.OrderLine;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrderLineRepository extends JpaRepository<OrderLineRepository, Long> {
+public interface OrderLineRepository extends JpaRepository<OrderLine, Long> {
 }
