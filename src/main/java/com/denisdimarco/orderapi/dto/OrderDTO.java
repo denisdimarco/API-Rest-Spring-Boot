e32a5f5d9d1f48a32ae790daf8048bf1762da43c
denisdimarco <denisdimarcoleda@gmail.com>
2021-11-19 01:35:23 -0300

    Fixed OrderDTO

--- src/main/java/com/denisdimarco/orderapi/dto/OrderDTO.java
+++ src/main/java/com/denisdimarco/orderapi/dto/OrderDTO.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Setter
 public class OrderDTO {
 
-    private long id;
+    private Long id;
     private String registerDate;
     private List<OrderLineDTO> lines;
     private Double total;
