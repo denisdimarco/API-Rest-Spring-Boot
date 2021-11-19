8679c893c3e40aca3cc71556f8244d7f4164c21f
denisdimarco <denisdimarcoleda@gmail.com>
2021-11-19 01:43:37 -0300

    Get ,Post & Delete integrated with OrderService

--- src/main/java/com/denisdimarco/orderapi/controller/OrderController.java
+++ src/main/java/com/denisdimarco/orderapi/controller/OrderController.java
@@ -3,10 +3,11 @@ package com.denisdimarco.orderapi.controller;
 import com.denisdimarco.orderapi.converter.OrderConverter;
 import com.denisdimarco.orderapi.dto.OrderDTO;
 import com.denisdimarco.orderapi.entity.Order;
+import com.denisdimarco.orderapi.service.OrderService;
 import com.denisdimarco.orderapi.utils.WrapperResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,32 +18,33 @@ public class OrderController {
 
     OrderConverter converter = new OrderConverter();
 
+    @Autowired
+    private OrderService orderService;
+
     @GetMapping("/orders")
     public ResponseEntity<WrapperResponse<List<OrderDTO>>> findAll(
             @RequestParam(name = "pageNumber", required = false, defaultValue = "0") int pageNumber,
             @RequestParam(name = "pageSize", required = false, defaultValue = "5") int pageSize
-    ){
+    ) {
 
         Pageable page = PageRequest.of(pageNumber, pageSize);
 
-        List<Order> orders = null; //TODO orderService.findAll(page);
-        return new WrapperResponse(true, "success", converter.fromEntity(orders))
+        List<Order> orders = orderService.findAll(page);
+        return new WrapperResponse<>(true, "success", converter.fromEntity(orders))
                 .createResponse();
     }
 
     @GetMapping("/orders/{id}")
-    public ResponseEntity<WrapperResponse<OrderDTO>> findById(@PathVariable(name="id") Long id) {
-        Order order = null; // orderService.findById(id);
-        return new WrapperResponse(true, "success", converter.fromEntity(order))
+    public ResponseEntity<WrapperResponse<OrderDTO>> findById(@PathVariable(name = "id") Long id) {
+        Order order = orderService.findById(id);
+        return new WrapperResponse<>(true, "success", converter.fromEntity(order))
                 .createResponse();
     }
 
     @PostMapping("/orders")
     public ResponseEntity<WrapperResponse<OrderDTO>> create(@RequestBody OrderDTO order) {
-
-        Order newOrder = null; // orderService.save(converter.fromDTO(order));
-
-        return new WrapperResponse(true, "success", converter.fromEntity(order))
+        Order newOrder = orderService.save(converter.fromDTO(order));
+        return new WrapperResponse<>(true, "success", converter.fromEntity(newOrder))
                 .createResponse();
     }
 
@@ -51,14 +53,13 @@ public class OrderController {
 
         Order newOrder = null; // orderService.save(converter.fromDTO(order));
 
-        return new WrapperResponse(true, "success", converter.fromEntity(order))
+        return new WrapperResponse<>(true, "success", converter.fromEntity(newOrder))
                 .createResponse();
     }
 
-    @DeleteMapping("/orders/id")
-    public ResponseEntity<?> delete(@PathVariable(name="id") Long id) {
-       // orderService.delete(id);
-
-        return new WrapperResponse(true, "success", null).createResponse();
+    @DeleteMapping("/orders/{id}")
+    public ResponseEntity<?> delete(@PathVariable(name = "id") Long id) {
+        orderService.delete(id);
+        return new WrapperResponse<>(true, "success", null).createResponse();
     }
 }
