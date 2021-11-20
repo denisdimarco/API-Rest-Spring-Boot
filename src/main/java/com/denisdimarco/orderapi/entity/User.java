d346f302eb4af33b7b6cce7bd92fa48bfac281b8
denisdimarco <denisdimarcoleda@gmail.com>
2021-11-20 17:12:18 -0300

    User entity done

---
+++ src/main/java/com/denisdimarco/orderapi/entity/User.java
@@ -0,0 +1,55 @@
+package com.denisdimarco.orderapi.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @Column(name="id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username", length = 30, nullable = false)
+    private String username;
+
+    @Column(name = "password", length = 150, nullable = false)
+    private String password;
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        User other = (User) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+}
+
+
